5710757454300d79517b04deda0958e7d61e19ba
jfeast <jordan.feast@outlook.com>
2025-11-03 21:47:54 +0000

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shinrin_yoku92/Chirpy
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+	const filepathroot = "."
+
+	mux := http.NewServeMux()
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathroot))))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain"+"; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	log.Printf("Starting server on port %s", port)
+	log.Printf("Serving files from %s", filepathroot)
+	log.Fatal(srv.ListenAndServe())
+}
