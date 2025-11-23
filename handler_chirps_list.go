c7231f7f281a8ac8096d4defd8378f17baeef208
jfeast <jordan.feast@outlook.com>
2025-11-23 14:22:34 +0000

    Added chirp search queries

--- handler_chirps_list.go
+++ handler_chirps_list.go
@@ -2,25 +2,70 @@ package main
 
 import (
 	"net/http"
+	"sort"
+
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerListChirps(w http.ResponseWriter, r *http.Request) {
-	chirps, err := cfg.db.ListChirps(r.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't list chirps", err)
-		return
-	}
+	authID := r.URL.Query().Get("author_id")
+	sortOrder := r.URL.Query().Get("sort")
 
-	var respChirps []Chirp
-	for _, c := range chirps {
-		respChirps = append(respChirps, Chirp{
-			ID:        c.ID,
-			CreatedAt: c.CreatedAt,
-			UpdatedAt: c.UpdatedAt,
-			Body:      c.Body,
-			UserID:    c.UserID,
-		})
-	}
+	if authID != "" {
+		userID, err := uuid.Parse(authID)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author_id", err)
+			return
+		}
 
-	respondWithJSON(w, http.StatusOK, respChirps)
+		chirps, err := cfg.db.ListChirpsByUserID(r.Context(), userID)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't list chirps", err)
+			return
+		}
+
+		var respChirps []Chirp
+		for _, c := range chirps {
+			respChirps = append(respChirps, Chirp{
+				ID:        c.ID,
+				CreatedAt: c.CreatedAt,
+				UpdatedAt: c.UpdatedAt,
+				Body:      c.Body,
+				UserID:    c.UserID,
+			})
+		}
+
+		if sortOrder == "desc" {
+			sort.Slice(respChirps, func(i, j int) bool {
+				return respChirps[i].CreatedAt.After(respChirps[j].CreatedAt)
+			})
+		}
+
+		respondWithJSON(w, http.StatusOK, respChirps)
+	} else {
+		chirps, err := cfg.db.ListChirps(r.Context())
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't list chirps", err)
+			return
+		}
+
+		var respChirps []Chirp
+		for _, c := range chirps {
+			respChirps = append(respChirps, Chirp{
+				ID:        c.ID,
+				CreatedAt: c.CreatedAt,
+				UpdatedAt: c.UpdatedAt,
+				Body:      c.Body,
+				UserID:    c.UserID,
+			})
+		}
+
+		if sortOrder == "desc" {
+			sort.Slice(respChirps, func(i, j int) bool {
+				return respChirps[i].CreatedAt.After(respChirps[j].CreatedAt)
+			})
+		}
+
+		respondWithJSON(w, http.StatusOK, respChirps)
+	}
 }
--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -14,6 +14,12 @@ SELECT *
 FROM chirps
 ORDER BY created_at ASC;
 
+-- name: ListChirpsByUserID :many
+SELECT *
+FROM chirps
+WHERE user_id = $1
+ORDER BY created_at ASC;
+
 -- name: GetChirpByID :one
 SELECT *
 FROM chirps
